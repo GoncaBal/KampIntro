2fd4a7d07bd41e25d42e59d6a77fb37fd147bc25
Gonca <75548809+GoncaBal@users.noreply.github.com>
2021-02-07 00:32:22 +0300

    Add files via upload

---
+++ GenericsMyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsMyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                values[i] = tempValues[i];
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Id:" + keys[i] + " İsim:" + values[i]);
+            }
+            
+        }
+    }
+}
---
+++ GenericsMyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace GenericsMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ogrenciler = new Dictionary<int, string>();
+            ogrenciler.Add(1, "Gonca");
+            
+
+            MyDictionary<int, string> ogrenciler2 = new MyDictionary<int, string>();
+            ogrenciler2.Add(1,"Reyhan");
+            ogrenciler2.Add(2, "Gonca");
+            ogrenciler2.Add(3, "Hatice");
+            ogrenciler2.Add(5, "Kenan");
+            ogrenciler2.List();
+        }
+    }
+}
